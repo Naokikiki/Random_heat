846fe22aedca8c687bebaa3877cb47ffa0b4b150
Naoki <naokiki0605@gmail.com>
2024-04-10 21:42:06 +0200

    some PDEs

---
+++ random_field/jitfailure-dolfin_expression_b34fbc674ce131653e6d72e3d0d8e3e2/dolfin_expression_b34fbc674ce131653e6d72e3d0d8e3e2.cpp
@@ -0,0 +1,100 @@
+
+// Based on https://gcc.gnu.org/wiki/Visibility
+#if defined _WIN32 || defined __CYGWIN__
+    #ifdef __GNUC__
+        #define DLL_EXPORT __attribute__ ((dllexport))
+    #else
+        #define DLL_EXPORT __declspec(dllexport)
+    #endif
+#else
+    #define DLL_EXPORT __attribute__ ((visibility ("default")))
+#endif
+
+#include <dolfin/function/Expression.h>
+#include <dolfin/math/basic.h>
+#include <Eigen/Dense>
+
+
+// cmath functions
+using std::cos;
+using std::sin;
+using std::tan;
+using std::acos;
+using std::asin;
+using std::atan;
+using std::atan2;
+using std::cosh;
+using std::sinh;
+using std::tanh;
+using std::exp;
+using std::frexp;
+using std::ldexp;
+using std::log;
+using std::log10;
+using std::modf;
+using std::pow;
+using std::sqrt;
+using std::ceil;
+using std::fabs;
+using std::floor;
+using std::fmod;
+using std::max;
+using std::min;
+
+const double pi = DOLFIN_PI;
+
+
+namespace dolfin
+{
+  class dolfin_expression_b34fbc674ce131653e6d72e3d0d8e3e2 : public Expression
+  {
+     public:
+       double w_j;
+double j;
+double alpha;
+
+
+       dolfin_expression_b34fbc674ce131653e6d72e3d0d8e3e2()
+       {
+            
+       }
+
+       void eval(Eigen::Ref<Eigen::VectorXd> values, Eigen::Ref<const Eigen::VectorXd> x) const override
+       {
+          values[0] = w_j /pow(j,alpha)*sin(j*pi*x);
+
+       }
+
+       void set_property(std::string name, double _value) override
+       {
+          if (name == "w_j") { w_j = _value; return; }          if (name == "j") { j = _value; return; }          if (name == "alpha") { alpha = _value; return; }
+       throw std::runtime_error("No such property");
+       }
+
+       double get_property(std::string name) const override
+       {
+          if (name == "w_j") return w_j;          if (name == "j") return j;          if (name == "alpha") return alpha;
+       throw std::runtime_error("No such property");
+       return 0.0;
+       }
+
+       void set_generic_function(std::string name, std::shared_ptr<dolfin::GenericFunction> _value) override
+       {
+
+       throw std::runtime_error("No such property");
+       }
+
+       std::shared_ptr<dolfin::GenericFunction> get_generic_function(std::string name) const override
+       {
+
+       throw std::runtime_error("No such property");
+       }
+
+  };
+}
+
+extern "C" DLL_EXPORT dolfin::Expression * create_dolfin_expression_b34fbc674ce131653e6d72e3d0d8e3e2()
+{
+  return new dolfin::dolfin_expression_b34fbc674ce131653e6d72e3d0d8e3e2;
+}
+
